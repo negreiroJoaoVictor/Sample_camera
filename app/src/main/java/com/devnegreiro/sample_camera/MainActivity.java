e6b77927da502fe98bc3f04557f2274240bb3d52
JoaoNegreiro <joaonegreiros@super.ufam.edu.br>
2020-07-28 14:56:51 -0400

    created a indent to take a picture

--- app/src/main/java/com/devnegreiro/sample_camera/MainActivity.java
+++ app/src/main/java/com/devnegreiro/sample_camera/MainActivity.java
@@ -2,13 +2,42 @@ package com.devnegreiro.sample_camera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_TakePicture = findViewById(R.id.button_takePicture);
+        btn_TakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
     }
+
+
+
+
+    private void dispatchTakePictureIntent(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+
+        }
+
+    }
+
 }
